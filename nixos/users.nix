1dbfc6f959613a6854cccb4b4c4ef8ae52c987d6
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-21 15:55:25 +0000

    setup authorised keys for root

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,8 +1,14 @@
 {config, ...}: {
-  users.users.user = {
-    initialPassword = "TODO";
-    isNormalUser = true;
-    openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD nima.kalantar"];
-    extraGroups = ["networkmanager" "wheel" "docker"];
+  users.users = {
+    root = {
+      initialPassword = "TODO";
+      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"];
+    };
+    user = {
+      initialPassword = "TODO";
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"];
+      extraGroups = ["networkmanager" "wheel" "docker"];
+    };
   };
 }
