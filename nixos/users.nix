6c3e861b8d611cb64a9caba67971d2d91db993dc
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-21 16:10:17 +0000

    add public keys for root and user

--- nixos/users.nix
+++ nixos/users.nix
@@ -2,12 +2,18 @@
   users.users = {
     root = {
       initialPassword = "TODO";
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8 nima.kalantar@focusrite.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD nima.kalantar@focusrite.com"
+      ];
     };
     user = {
       initialPassword = "TODO";
       isNormalUser = true;
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8 nima.kalantar@focusrite.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD nima.kalantar@focusrite.com"
+      ];
       extraGroups = ["networkmanager" "wheel" "docker"];
     };
   };
