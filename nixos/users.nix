f2f5aa7019df58bb7959a32de127d03016fe8574
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-22 10:41:38 +0000

    comment out TODO

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,14 +1,14 @@
 {config, ...}: {
   users.users = {
     root = {
-      initialPassword = "TODO";
+      # initialPassword = "TODO";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8 nima.kalantar@focusrite.com"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD nima.kalantar@focusrite.com"
       ];
     };
     user = {
-      initialPassword = "TODO";
+      # initialPassword = "TODO";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8 nima.kalantar@focusrite.com"
