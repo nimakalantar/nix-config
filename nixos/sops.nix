9e19a0dbf892947d76f85377fe3afefa4a1a7f7b
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-25 21:23:44 +0000

    drop secret inits

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -3,8 +3,4 @@
   sops.age.sshKeyPaths = ["/etc/ssh/id_ed25519"];
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
-  sops.secrets.users = {};
-  sops.secrets.git = {};
-  sops.secrets.locale = {};
-  sops.secrets.networks = {};
 }
