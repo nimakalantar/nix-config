b85121d37d704e19bfd03a47c701de6a48d62686
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-27 17:01:32 +0000

    disable networkmanager

--- nixos/users.nix
+++ nixos/users.nix
@@ -18,7 +18,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"
       ];
-      extraGroups = ["networkmanager" "wheel" "docker"];
+      extraGroups = [ "wheel" "docker" ];
     };
   };
 }
