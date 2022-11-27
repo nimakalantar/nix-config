c2fe77ecf6e691aaa5f2b801d3e47d447757f95b
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-27 14:58:56 +0000

    add darwin home manager config

---
+++ home-manager/mac.nix
@@ -0,0 +1,49 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/home-manager):
+    outputs.homeManagerModules.git
+    outputs.homeManagerModules.ssh
+    outputs.homeManagerModules.pkgs
+
+    # Or modules exported from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModules.default
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home = {
+    username = "nima.kalantar";
+    homeDirectory = "/home/nima.kalantar";
+  };
+
+  home.packages = with pkgs; [
+    chezmoi
+  ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "22.05";
+}
