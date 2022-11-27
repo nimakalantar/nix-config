25186c546f10dd68afdc5aca0ce8d7953c002c2e
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-27 15:00:45 +0000

    wip: add initial darwin config

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ To deploy remotely:
 deploy -s
 ```
 
+To rebuild Darwin config:
+```
+nix build --extra-experimental-features nix-command --extra-experimental-features flakes .#FF0523
+```
+
 To rebuild NixOS config:
 ```
 sudo nixos-rebuild switch --flake .#nuc
@@ -26,6 +31,7 @@ sops secrets/hosts/nuc.yaml
 - [x] flake.lock update mechanism (renovate)
 - [x] Managed deployments (deploy-rs)
 - [x] Secrets management (sops-nix)
+- [x] Darwin configuration (nix-darwin)
 - [ ] Use BTRFS for /root
 - [ ] Opt-in persistance (/tmpfs RAM disk)
 - [ ] Custom media
--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
     hardware.url = "github:nixos/nixos-hardware";
     impermanence.url = "github:nix-community/impermanence";
 
+    darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,36 +28,29 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
+    darwin,
     home-manager,
     sops-nix,
     deploy-rs,
     ...
   } @ inputs: let
     inherit (self) outputs;
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    deploy = {
-      nodes = {
-        nuc = {
-          hostname = "nuc";
-          profiles.system = {
-            sshUser = "root";
-            user = "root";
-            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nuc;
-          };
-        };
-      };
-    };
-
-    # This is highly advised, and will prevent many possible mistakes
-    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-
-    formatter.x86_64-linux = pkgs.alejandra;
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+  in
+  rec {
+    formatters = forAllSystems (system:
+      with (nixpkgs.legacyPackages.${system}); [ alejandra ]
+    );
 
     # Devshell for bootstrapping
     # Acessible through 'nix develop' or 'nix-shell' (legacy)
-    devShells.x86_64-linux = import ./shell.nix {inherit pkgs;};
+    devShells = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in import ./shell.nix { inherit pkgs; }
+    );
 
     # Reusable nixos modules you might want to export
     nixosModules = import ./modules/nixos;
@@ -69,15 +67,50 @@
       };
     };
 
+    darwinConfigurations."FF0523" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [ ./darwin/configuration.nix ];
+    };
+
     homeConfigurations = {
       "user@nuc" = home-manager.lib.homeManagerConfiguration {
-        pkgs = pkgs; # Home-manager requires 'pkgs' instance
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main home-manager configuration file <
-          ./home-manager/home.nix
+          ./home-manager/nuc.nix
+        ];
+      };
+      "nima.kalantar@FF0523" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/mac.nix
         ];
       };
     };
+
+    deploy.nodes = {
+      nuc = {
+        hostname = "nuc";
+        profiles.system = {
+          sshUser = "root";
+          user = "root";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nuc;
+        };
+      };
+      mac = {
+        hostname = "FF0523";
+        profiles.system = {
+          sshUser = "nima.kalantar";
+          user = "nima.kalantar";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.nuc;
+        };
+      };
+    };
+
+    # This is highly advised, and will prevent many possible mistakes
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   };
 }
