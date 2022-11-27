bb42c736f478012597bae3567241415d693e0577
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-27 17:04:58 +0000

    update ssh config matchblocks

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -5,6 +5,18 @@
 }: {
   programs.ssh = {
     enable = true;
+    matchBlocks = {
+      "nuc" = {
+        user = "user";
+      };
+      "nuc.lan" = {
+        hostname = "192.168.1.121";
+        user = "user";
+      };
+      "*.github.com" = {
+        user = "git";
+      };
+    };
     extraConfig = "AddKeysToAgent yes";
   };
 }
