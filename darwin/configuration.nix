3c3f410275e247f7d5f52915b5c8ab09411b3191
Nima Kalantar <nima.kalantar@focusrite.com>
2022-11-30 13:45:26 +0000

    add boilerplate flakes config

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,15 +1,37 @@
 {
   inputs,
+  outputs,
+  lib,
   config,
   pkgs,
   ...
 }: {
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mapAttrs (_: value: {flake = value;}) inputs;
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+
+    # Automatic garbage collection
+    gc.automatic = true;
+
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+      allowed-users = ["root"];
+    };
+  };
+
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   environment.darwinConfig = "$HOME/nix-config/darwin/configuration.nix";
-
-  # Automatic garbage collection
-  nix.gc.automatic = true;
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
