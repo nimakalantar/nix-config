77b3d0ebc5f08953ba1716d1372a465f73a72337
Nima Kalantar <nima.kalantar@focusrite.com>
2022-12-01 19:11:02 +0000

    temp

--- nixos/network.nix
+++ nixos/network.nix
@@ -1,23 +1,14 @@
 {config, ...}: {
-  networking.usePredictableInterfaceNames = true;
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Direct ethernet connection
-  networking.interfaces.enp112s0 = {
-    useDHCP = false;
-    ipv4.addresses = [
-      {
-        address = "10.0.0.1";
-        prefixLength = 28;
-      }
-    ];
+  sops.secrets = {
+    "networks/home/ssid" = {};
+    "networks/home/password" = {};
   };
 
-  # networking.wireless.networks = {
-  #   "" = {
-  #     psk = "";
-  #   };
-  # };
+  networking.wireless.iwd.enable = true;
+
+  networking.wireless.networks = {
+    ssid = {
+      psk = psk;
+    };
+  };
 }
