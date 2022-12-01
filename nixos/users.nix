cd003d3ab6af876136c4a41bc63f6fad7b1140ed
Nima Kalantar <nima.kalantar@focusrite.com>
2022-12-01 19:52:35 +0000

    setup password files

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,18 +1,19 @@
 {config, ...}: {
-  # sops.secrets = {
-  #   "users/root".neededForUsers = true;
-  #   "users/user".neededForUsers = true;
-  # };
+  sops.secrets = {
+    "root_pass".neededForUsers = true;
+    "user_pass".neededForUsers = true;
+  };
+
   users.users = {
     root = {
-      # passwordFile = config.sops.secrets.root.path;;
+      passwordFile = config.sops.secrets.root_pass.path;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"
       ];
     };
     user = {
-      # passwordFile = config.sops.secrets.user.path;;
+      passwordFile = config.sops.secrets.user_pass.path;
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8"
