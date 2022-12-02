4e2ce57108f01c0a3b7911fe4e07d150e9edad28
Nima Kalantar <nima.kalantar@focusrite.com>
2022-12-02 22:41:37 +0000

    immutable users

--- hosts/nuc/users.nix
+++ hosts/nuc/users.nix
@@ -4,22 +4,25 @@
     "user_pass".neededForUsers = true;
   };
 
-  users.users = {
-    root = {
-      passwordFile = config.sops.secrets.root_pass.path;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"
-      ];
-    };
-    user = {
-      passwordFile = config.sops.secrets.user_pass.path;
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"
-      ];
-      extraGroups = ["wheel" "docker"];
+  users = {
+    mutableUsers = false;
+    users = {
+      root = {
+        passwordFile = config.sops.secrets.root_pass.path;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"
+        ];
+      };
+      user = {
+        passwordFile = config.sops.secrets.user_pass.path;
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqN6QIStiPMRYSQZsLVmhEtQrOToriIEPuPw+q1THp8"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp007s4hFGCvBDiBwDzY45KZfyjUEcE34nE5W2eYPGD"
+        ];
+        extraGroups = ["wheel" "docker"];
+      };
     };
   };
 }
