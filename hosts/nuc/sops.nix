de27884223bfb6be106968f2135867efeebb8270
Nima Kalantar <nima.kalantar@focusrite.com>
2022-12-02 22:07:22 +0000

    fix path

--- hosts/nuc/sops.nix
+++ hosts/nuc/sops.nix
@@ -1,5 +1,5 @@
 {config, ...}: {
-  sops.defaultSopsFile = "../../secrets/nuc.yaml";
+  sops.defaultSopsFile = ../../secrets/nuc.yaml;
   sops.age.sshKeyPaths = ["/etc/ssh/id_ed25519"];
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
