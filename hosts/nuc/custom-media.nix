ef359dea8ebf8a359c7e963d108f1710b78e7d0a
Nima Kalantar <nima.kalantar@focusrite.com>
2022-12-04 17:01:53 +0000

    add git to iso

--- hosts/nuc/custom-media.nix
+++ hosts/nuc/custom-media.nix
@@ -7,5 +7,13 @@
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
   ];
 
+  # use the latest Linux kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # compress with zstd
   isoImage.compressImage = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+  ]
 }
