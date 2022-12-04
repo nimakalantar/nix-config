bace6198dd0b9af2671016d8e5129feb18f2bcdc
Nima Kalantar <nima.kalantar@focusrite.com>
2022-12-04 01:18:51 +0000

    add swap and use btrfs subvolumes

--- hosts/nuc/install.sh
+++ hosts/nuc/install.sh
@@ -2,7 +2,7 @@
 #!/usr/bin/env bash
 
 # The name of the install drive
-DISK=/dev/disk/by-id/nvme-Samsung_SSD_980_1TB_S649NL0T815408K
+DISK=/dev/nvme0n1
 # Disk encryption password
 PASSWORD="dummy"
 
@@ -11,21 +11,22 @@ sudo -i parted $DISK -- mklabel gpt
 # Boot partition
 sudo -i parted $DISK -- mkpart ESP vfat 1MiB 512MiB
 sudo -i parted $DISK -- set 1 boot on
+# Swap partition
+sudo -i parted $DISK -- mkpart primary linux-swap 512MiB 8.5GiB
 # Primary partition
 sudo -i parted $DISK -- mkpart primary 8.5GiB 100%
 
 # Setup encryption on the primary partition.
-sudo sh -c "echo $PASSWORD | cryptsetup luksFormat /dev/disk/by-partlabel/primary"
+sudo sh -c "echo $PASSWORD | cryptsetup luksFormat ${DISK}p3"
 # Mount a decrypted version of the encrypted primary partition.
-sudo sh -c "echo $PASSWORD | cryptsetup luksOpen /dev/disk/by-partlabel/primary nixos-decrypted"
+sudo sh -c "echo $PASSWORD | cryptsetup luksOpen ${DISK}p3 nixos-decrypted"
 
-# Swap partition
-sudo -i parted $DISK -- mkpart primary linux-swap 512MiB 8.5GiB
 
 # Format the boot partition.
 sudo -i mkfs.vfat -n boot /dev/disk/by-partlabel/ESP
 # Format the swap partition
 sudo -i mkswap /dev/disk/by-partlabel/swap
+sudo -i swapon /dev/disk/by-partlabel/swap
 # Format the decrypted version of the primary partition.
 sudo -i mkfs.btrfs -L nixos /dev/mapper/nixos-decrypted
 
@@ -33,16 +34,29 @@ sudo -i mkfs.btrfs -L nixos /dev/mapper/nixos-decrypted
 sleep 4
 
 # Mount your root file system
-sudo -i mount -t tmpfs none /mnt
-sudo -i mkdir -p /mnt/{boot,nix,etc/nixos,var/log}
-sudo -i mount -o noatime /dev/disk/by-label/boot /mnt/boot
-sudo -i swapon /dev/disk/by-partlabel/swap
-sudo -i mount -o noatime /dev/disk/by-label/nixos /mnt
+sudo -i mount -t btrfs /dev/mapper/nixos-decrypted /mnt
+# We first create the subvolumes outlined above:
+sudo -i btrfs subvolume create /mnt/root
+sudo -i btrfs subvolume create /mnt/home
+sudo -i btrfs subvolume create /mnt/nix
+sudo -i btrfs subvolume create /mnt/persist
+sudo -i btrfs subvolume create /mnt/log
+# We then take an empty *readonly* snapshot of the root subvolume,
+# which we'll eventually rollback to on every boot.
+sudo -i btrfs subvolume snapshot -r /mnt/root /mnt/root-blank
+sudo -i umount /mnt
 
+# Mount root subvolume
+mount -o subvol=root,compress=zstd,noatime /dev/mapper/nixos-decrypted /mnt
 # Create a directory for persistent directories
-sudo -i mkdir -p /mnt/nix/persist/{etc/nixos,var/log,home}
-
-# Bind mount the persistent configuration / logs
-sudo -i mount -o bind /mnt/nix/persist/etc/nixos /mnt/etc/nixos
-sudo -i mount -o bind /mnt/nix/persist/var/log /mnt/var/log
-sudo -i mount -o bind /mnt/nix/persist/home /mnt/home
+sudo -i mkdir -p /mnt/{boot,home,nix,persist,var/log}
+# Mount home subvolume
+sudo -i mount -o subvol=home,compress=zstd,noatime /dev/mapper/nixos-decrypted /mnt/home
+# Mount nix subvolume
+sudo -i mount -o subvol=nix,compress=zstd,noatime /dev/mapper/nixos-decrypted /mnt/nix
+# Mount persist subvolume
+sudo -i mount -o subvol=persist,compress=zstd,noatime /dev/mapper/nixos-decrypted /mnt/persist
+# Mount log subvolume
+sudo -i mount -o subvol=log,compress=zstd,noatime /dev/mapper/nixos-decrypted /mnt/var/log
+# Mount boot subvolume
+sudo -i mount "$DISK"p1 /mnt/boot
