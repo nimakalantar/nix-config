eb87adccb41d6af322c33eef0281b1143293980c
Nima Kalantar <nima.kalantar@focusrite.com>
2022-12-04 17:05:37 +0000

    fix

--- hosts/nuc/custom-media.nix
+++ hosts/nuc/custom-media.nix
@@ -9,11 +9,13 @@
 
   # use the latest Linux kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  # zfs package is broken so this is required
+  nixpkgs.config.allowBroken = true;
 
   # compress with zstd
   isoImage.compressImage = true;
 
   environment.systemPackages = with pkgs; [
     git
-  ]
+  ];
 }
